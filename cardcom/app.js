d0c9c03eb4b45ea6151c16d04ed0010cfb0521f6
Ron Lin <ron@card.com>
2014-03-05 11:17:17 -0800

    cardcom app created

---
+++ cardcom/app.js
@@ -0,0 +1,36 @@
+(function() {
+
+  // not using SIDE_BAR_HREF
+  var SIDE_BAR_HREF = "https://www.wikipedia.org",
+      SIDE_BAR_REGEX = /_sidebar$/;
+
+  return {
+    events: {
+      'app.activated': 'init',
+      'pane.activated': 'paneActivated'
+    },
+
+    init: function(data){ //load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
+      if (data.firstLoad && SIDE_BAR_REGEX.test(this.currentLocation())) {
+        this.showIframe({ width: '300px', height: '260px' });
+      }
+    },
+
+    paneActivated: function(data) { //load content if app is at top_bar and nav_bar
+      if (data.firstLoad) {
+        this.showIframe({ width: '1024px', height: '500px' });
+      }
+    },
+
+    showIframe: function(dimensions) {
+      // ticket should be used later to share information with cardcom agent dash
+      var ticket = this.ticket();
+      var cUser = this.currentUser();
+      var url="//www.card.com/zendesk/agent?uid=" + cUser.id();
+      this.switchTo('iframePage', {
+        dimension: dimensions,
+        href: url
+      });
+    }
+  };
+}());
