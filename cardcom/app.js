23fa9bacd9579f967f147f6a2b959dcf1c375ecf
Ron Lin <ron@card.com>
2014-03-21 17:47:40 -0700

    Passing ticket_id to the agent dashboard

--- cardcom/app.js
+++ cardcom/app.js
@@ -25,9 +25,23 @@
     showIframe: function(dimensions) {
       // @todo: ticket should be used to share information with cardcom agent dash
       // http://developer.zendesk.com/documentation/apps/reference/data.html#user-object
-      var agent = this.currentUser();
-      var user = this.user();
-      var url="//www.card.com/zendesk/agent?uid=" + user.externalId() + "&email=" + user.email() + "&zid=" + user.id();
+      var ticket;
+      var user;
+      var url;
+      // var agent = this.currentUser();
+      // @todo: add phone number parameter
+      if (this.hasOwnProperty('ticket')) {
+        ticket = this.ticket();
+        user = ticket.requester();
+        url="//www.card.com/zendesk/agent?uid=" + user.externalId() + "&email=" + user.email() + "&zid=" + user.id() + "&tid=" + ticket.id();
+      }
+      else if (this.hasOwnProperty('user')) {
+        user = this.user();
+        url="//www.card.com/zendesk/agent?uid=" + user.externalId() + "&email=" + user.email() + "&zid=" + user.id();
+      }
+      else {
+        url="//www.card.com/zendesk/agent?uid=0";
+      }
       this.switchTo('iframePage', {
         dimension: dimensions,
         href: url
